44aa56f43c65967fd389d14760b672de7560a20d
ngadiono <ondoelratz@gmail.com>
2018-07-20 18:27:47 +0700

    add thumbnail

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({album}) => {
+  const { title, artist, thumbnail_image } = album;
+  const { thumbnailStyle, headerContentStyle } = styles;
+
   return (
     <Card>
       <CardSection>
-        <Text>{props.album.title}</Text>
+        <View>
+          <Image
+            style={thumbnailStyle}
+            source={{uri: thumbnail_image}} />
+        </View>
+        <View style={headerContentStyle}>
+          <Text>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
       </CardSection>
     </Card>
   )
+}
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: ''
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50,
+    marginRight: 10
+  }
 }
 
 export default AlbumDetail;
